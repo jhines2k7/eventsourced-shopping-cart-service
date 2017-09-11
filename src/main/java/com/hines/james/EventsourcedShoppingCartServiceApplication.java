1537de67f34f518d480ce468c716446e4369245f
James Hines <jameshines10@gmail.com>
2017-09-11 07:42:05 -0400

    Initil commit

---
+++ src/main/java/com/hines/james/EventsourcedShoppingCartServiceApplication.java
@@ -0,0 +1,12 @@
+package com.hines.james;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsourcedShoppingCartServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsourcedShoppingCartServiceApplication.class, args);
+	}
+}
